9057635f0a072ba954526a6d732454c6a82d1631
bixuelei <bxlgermany@outlook.com>
2022-05-15 17:59:30 +0200

    init

---
+++ synthetic_motors_generate.py
@@ -0,0 +1,224 @@
+import os
+import bpy
+import numpy as np
+import random
+import math
+
+'''
+Value-distribution:
+    Type A_Type 1 :
+                    Bottom Length = 4.00 - 8.00  step = 0.03
+                    Sub Bottom Length = 0.6 - 2  setp = 0.03
+                    Lower Gear Dia = 3.5 - 4.5  step = 0.03
+                    Lower Gear Position = 3.6 - 4.2
+                    Upper Gear Dia = 5 - 6.5
+                    Bolts Type = 'mf_Bit_Slot', 'mf_Bit_Torx', 'mf_Bit_Corss',
+                    Bolt Orientation = 'mf_all_same', 'mf_all_random'
+                    Position of Bolt 1 on lower gear = 190 - 230    step = 5
+                    Position of Bolts on lower gear = 320 - 350    step = 5
+                    Random of Position around lower gear = True/False
+                    Number of Bolts = 1, 2, 3
+                    Position of bolt 1 on larger gear = 0 - 105    step = 1
+                    Position of bolt 2 on larger gear = 106 - 210    step = 1
+The rule I used now:
+                    Each changed parameter in changed value + the other parameter in default values  ->  3 groups with different type of bolts
+'''
+def create_Motor_Obj_A(Extension_Type_A, Bolt_type_mode, changed_parameter, Upper_Bolt_Nummber, save_dir) :         # Motor_type = ['Type_A', 'Type_B']  Type_A_Extension = ['Type_1', 'Type_2']
+   
+    try :
+        if bpy.data.objects['Cube'] :       # Delete the Cube object and Lamp object from Blender's intialization
+            bpy.data.objects.remove(bpy.data.objects['Cube'])
+        if bpy.data.objects['Light'] :
+            bpy.data.objects.remove(bpy.data.objects['Light'])
+    except KeyError :
+        pass
+
+    try :
+        if bpy.data.objects['Motor']:
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+    except KeyError:
+        pass
+    
+    Bolts_Type = ['mf_Bit_Slot', 'mf_Bit_Torx', 'mf_Bit_Corss']
+    if Extension_Type_A == 'mf_Extension_Type_1' :
+        if Bolt_type_mode == 'Torx' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_Extension_Type_1', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type='mf_Bit_Torx', save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+        elif Bolt_type_mode == 'Random' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_Extension_Type_1', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type=random.choice(Bolts_Type), save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+
+    elif Extension_Type_A == 'mf_Extension_Type_2' :
+        if Bolt_type_mode == 'Torx' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_Extension_Type_2', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type='mf_Bit_Torx', save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+        elif Bolt_type_mode == 'Random' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_Extension_Type_2', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type=random.choice(Bolts_Type), save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+
+    elif Extension_Type_A == 'mf_Extension_Type_None' :
+        if Bolt_type_mode == 'Torx' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_None', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type='mf_Bit_Torx', save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+        elif Bolt_type_mode == 'Random' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Lower_Gear_Bolt_Random=True,  mf_Upper_Gear_Bolt_Random=True, temp_save=True, mf_Top_Type='mf_Top_Type_A',
+                    mf_Extension_Type_A='mf_None', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'],
+                    mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Upper_Gear_Dia=changed_parameter['changed_UGD'], mf_Upper_Bolt_Nummber=Upper_Bolt_Nummber, 
+                    mf_Bolt_Orientation='mf_all_random', mf_Bit_Type=random.choice(Bolts_Type), save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+
+
+'''
+Value-distribution:
+    Type B :
+                    Bottom Length = 4.00 - 8.00  step = 0.03
+                    Sub Bottom Length = 0.6 - 2  setp = 0.03
+                    Lower Gear Dia = 3.5 - 4.5  step = 0.03
+                    Lower Gear Position = 3.6 - 4.2
+                    Height of Extension left = 6.3 - 7.5
+                    Height of Extension right = 3 - 6
+                    Bolts Type = 'mf_Bit_Slot', 'mf_Bit_Torx', 'mf_Bit_Corss',
+                    Bolt Orientation = 'mf_all_same', 'mf_all_random'
+                    Position of Bolt at right = 1.7 - 4    step = 0.03
+                    Position of bolt 1 on larger gear = 210 - 225    step = 1
+                    Position of bolt 2 on larger gear = 70 - 110    step = 1
+The rule I used now:
+                    Each changed parameter in changed value + the other parameter in default values  ->  3 groups with different type of bolts
+'''
+def create_Motor_Obj_B(Extension_Type_B, Bolt_type_mode, changed_parameter, Gear_Bolt_Nummber_B, save_dir) :         # Motor_type = ['Type_A', 'Type_B']  Type_A_Extension = ['Type_1', 'Type_2']
+
+    try :
+        if bpy.data.objects['Cube'] :       # Delete the Cube object and Lamp object from Blender's intialization
+            bpy.data.objects.remove(bpy.data.objects['Cube'])
+        if bpy.data.objects['Light'] :
+            bpy.data.objects.remove(bpy.data.objects['Light'])
+    except KeyError :
+        pass
+
+    try :
+        if bpy.data.objects['Motor']:
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+    except KeyError:
+        pass
+
+    Bolts_Type = ['mf_Bit_Slot', 'mf_Bit_Torx', 'mf_Bit_Corss']
+    if Extension_Type_B == 'Extension_Type_1' :
+        if Bolt_type_mode == 'Torx' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Gear_Bolt_Random_B=True, temp_save=True, mf_Top_Type='mf_Top_Type_B',
+                mf_Extension_Type_B='mf_Extension_Type_1', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'], 
+                mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Gear_Bolt_Nummber_B='2', mf_Type_B_Height_1=changed_parameter['changed_HEL'], 
+                mf_Type_B_Height_2=changed_parameter['changed_HER'], mf_Gear_Bolt_Right_B=changed_parameter['changed_GBR'],
+                mf_Bolt_Orientation='mf_all_random', mf_Bit_Type='mf_Bit_Torx', save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+        elif Bolt_type_mode == 'random' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Gear_Bolt_Random_B=True, temp_save=True, mf_Top_Type='mf_Top_Type_B',
+                mf_Extension_Type_B='mf_Extension_Type_1', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'], 
+                mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Gear_Bolt_Nummber_B='2', mf_Type_B_Height_1=changed_parameter['changed_HEL'], 
+                mf_Type_B_Height_2=changed_parameter['changed_HER'], mf_Gear_Bolt_Right_B=changed_parameter['changed_GBR'],
+                mf_Bolt_Orientation='mf_all_random', mf_Bit_Type=random.choice(Bolts_Type), save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+    
+    elif Extension_Type_B == 'Extension_Type_None' :
+        if Bolt_type_mode == 'Torx' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Gear_Bolt_Random_B=True, temp_save=True, mf_Top_Type='mf_Top_Type_B',
+                mf_Extension_Type_B='mf_None', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'], 
+                mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Gear_Bolt_Nummber_B=Gear_Bolt_Nummber_B, mf_Type_B_Height_1=changed_parameter['changed_HEL'], 
+                mf_Type_B_Height_2=changed_parameter['changed_HER'], mf_Gear_Bolt_Right_B=changed_parameter['changed_GBR'],
+                mf_Bolt_Orientation='mf_all_random', mf_Bit_Type='mf_Bit_Torx', save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+        elif Bolt_type_mode == 'Random' :
+            bpy.ops.mesh.add_motor(change=True, mf_Color_Render=True, mf_Gear_Bolt_Random_B=True, temp_save=True, mf_Top_Type='mf_Top_Type_B',
+                mf_Extension_Type_B='mf_None', mf_Bottom_Length=changed_parameter['changed_BL'], mf_Sub_Bottom_Length=changed_parameter['changed_SBL'], mf_Lower_Gear_Dia=changed_parameter['changed_LGD'], 
+                mf_Lower_Gear_Position=changed_parameter['changed_LGP'], mf_Gear_Bolt_Nummber_B=Gear_Bolt_Nummber_B, mf_Type_B_Height_1=changed_parameter['changed_HEL'], 
+                mf_Type_B_Height_2=changed_parameter['changed_HER'], mf_Gear_Bolt_Right_B=changed_parameter['changed_GBR'],
+                mf_Bolt_Orientation='mf_all_random', mf_Bit_Type=random.choice(Bolts_Type), save_path=save_dir)
+            bpy.data.objects.remove(bpy.data.objects['Motor'])
+
+def main(num_motor):
+    BASE_DIR='/Users/bixuelei/Master/Motors/'
+    save_dir_TypeA1_origial = BASE_DIR+'TypeA1'
+    if not os.path.exists(save_dir_TypeA1_origial):
+        os.makedirs(save_dir_TypeA1_origial)
+
+    save_dir_TypeA2_original = BASE_DIR+'TypeA2'
+    if not os.path.exists(save_dir_TypeA2_original):
+        os.makedirs(save_dir_TypeA2_original)
+
+    save_dir_TypeANone_original = BASE_DIR+'TypeANone'
+    if not os.path.exists(save_dir_TypeANone_original):
+        os.makedirs(save_dir_TypeANone_original)
+
+    save_dir_TypeB1_origial = BASE_DIR+'TypeB1'
+    if not os.path.exists(save_dir_TypeB1_origial):
+        os.makedirs(save_dir_TypeB1_origial)
+
+    save_dir_TypeBNone_origial = BASE_DIR+'TypeBNone'
+    if not os.path.exists(save_dir_TypeBNone_origial):
+        os.makedirs(save_dir_TypeBNone_origial)
+
+
+    Upper_Bolt_Nummber_A = ['1', '2','2','3']
+    Upper_Bolt_Nummber_B = ['2','3']
+    for _ in range(int(num_motor/5)):
+        changed_parameter_A = {}
+        changed_parameter_A['changed_BL'] = float(np.random.uniform(4.0, 8.0, 1))          # Bottom_Length
+        changed_parameter_A['changed_SBL'] = float(np.random.uniform(0.6, 2.0, 1))         # Sub_Bottom_Length
+        changed_parameter_A['changed_LGD'] = float(np.random.uniform(3.5, 4.5, 1))         # Lower_Gear_Diameter
+        changed_parameter_A['changed_LGP'] = float(np.random.uniform(3.6, 4.2, 1))         # Lower_Gear_Position
+        changed_parameter_A['changed_UGD'] = float(np.random.uniform(5, 6.5, 1)) 
+        num_bolts_A=random.choice(Upper_Bolt_Nummber_A)
+        ###########################################  
+        #   Create Type A Extension 1 Motor       #
+        ###########################################
+        #create_Motor_Obj_A(Extension_Type_A = 'mf_Extension_Type_1', Bolt_type_mode = 'Torx', changed_parameter = changed_parameter_A, Upper_Bolt_Nummber=num_bolts_A, save_dir = save_dir_TypeA1_origial)
+
+        ###########################################  
+        #   Create Type A Extension 2 Motor       #
+        ###########################################
+        #create_Motor_Obj_A(Extension_Type_A = 'mf_Extension_Type_2', Bolt_type_mode = 'Torx', changed_parameter = changed_parameter_A, Upper_Bolt_Nummber=num_bolts_A, save_dir =save_dir_TypeA2_original)
+
+        ###########################################  
+        #   Create Type A Extension None Motor    #
+        ###########################################
+        #create_Motor_Obj_A(Extension_Type_A = 'mf_Extension_Type_None', Bolt_type_mode = 'Torx', changed_parameter = changed_parameter_A, Upper_Bolt_Nummber=num_bolts_A, save_dir = save_dir_TypeANone_original)
+
+
+        changed_parameter = {}
+        changed_parameter['changed_BL'] = float(np.random.uniform(4.0, 8.0, 1))          # Bottom_Length
+        changed_parameter['changed_SBL'] = float(np.random.uniform(0.6, 2.0, 1))         # Sub_Bottom_Length
+        changed_parameter['changed_LGD'] = float(np.random.uniform(3.5, 4.5, 1))         # Lower_Gear_Diameter
+        changed_parameter['changed_LGP'] = float(np.random.uniform(3.6, 4.2, 1))         # Lower_Gear_Position
+        changed_parameter['changed_GBR'] = float(np.random.uniform(1.7, 4.0, 1))         # Gear_Bolt_Right_B
+        changed_parameter['changed_HEL'] = float(np.random.uniform(6.3, 7.5, 1))         # Height of Extension left
+        changed_parameter['changed_HER'] = float(np.random.uniform(3.0, 6.0, 1))         # Height of Extension right
+        num_bolts_B=random.choice(Upper_Bolt_Nummber_B)
+        ###########################################  
+        #   Create Type B Extension 1 Motor       #
+        ###########################################
+        #create_Motor_Obj_B(Extension_Type_B = 'Extension_Type_1', Bolt_type_mode = 'Torx', changed_parameter = changed_parameter, Gear_Bolt_Nummber_B= Upper_Bolt_Nummber_B[0] , save_dir = save_dir_TypeB1_origial)      # Bolt_type_mode = ['Random', 'Torx']
+
+        ###########################################  
+        #   Create Type B Extension None Motor    #
+        ###########################################
+        create_Motor_Obj_B(Extension_Type_B = 'Extension_Type_None', Bolt_type_mode = 'Torx', changed_parameter = changed_parameter, Gear_Bolt_Nummber_B= num_bolts_B, save_dir = save_dir_TypeBNone_origial)
+
+
+
+if __name__ == '__main__':
+    num_motor_for_dataset=1000      #   paramater to set dataset size
+    main(num_motor=num_motor_for_dataset)
